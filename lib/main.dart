cc2d30d3314ebb057bd35e1a9530c0c4e2335b23
nineebibi <149037540+nineebibi@users.noreply.github.com>
2024-12-16 11:36:35 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -16,117 +16,107 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             centerTitle: true,
-            title:Image.asset('assets/image/honda_logo.png',width: 70,),
-
-
-            // leading: IconButton(onPressed: () {debugPrint('Testiconmenu');}, icon: const Icon(Icons.menu)),
-            // actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.flag_sharp)),
-            //           IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
-            // ],
+            title: Text('EV Charging',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
           ),
-          body: Column(
-            children: [
-              Image.asset('assets/image/honda.jpg'),
-              Text('ขอบคุณที่ใช้บริการ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-              Text('เรายินที่ได้ใช้บิรการกับเรา Honda\n\n',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-
-              Row(children: [Text('สรุปรายละเอียดการสั่งซื้อ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))],),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.calendar_month)),
-                Text('วันที่รับรถ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                Spacer(),Text('15/12/2567',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.place)),
-                Text('สถานที่รับรถ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                Spacer(),Text('สยามพารากอน',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.price_change_sharp)),
-                Text('เงินมัดจำที่ต้องจ่าย',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                Spacer(),Text('500,000 บาท',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.car_rental)),
-                Text('รุ่นของรถยนต์',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                Spacer(),Text('Honda Civic RS 2024',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                ],
-              ),
-              // Row(
-              //   children: [
-              //     ElevatedButton(onPressed: () {debugPrint('Eiei');}, child: Text('SUBMIT')),
-              //     IconButton(onPressed: () {}, icon: Icon(Icons.favorite),iconSize: 40,),
-              //   ],
-              // ),
-            ],
+          body: Padding(padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              Text('99%',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              Text('99%',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              Text('99%',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ],
+                      ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
+          // body: Padding(
+          //   padding: const EdgeInsets.all(10.0),
+          //   child: Column(
+          //     children: [
+          //       Image.asset('assets/image/honda.jpg'),
+          //       Text('ขอบคุณที่ใช้บริการ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //       Text('เรายินที่ได้ใช้บิรการกับเรา Honda\n\n',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //       Card(
+          //         child: Column(
+          //           children: [
+          //       Row(children: [Text('สรุปรายละเอียดการสั่งซื้อ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))],),
+          //       Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.calendar_month)),
+          //         Text('วันที่รับรถ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         Spacer(),Text('15/12/2567',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         ],
+          //       ),
+          //       Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.place)),
+          //         Text('สถานที่รับรถ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         Spacer(),Text('สยามพารากอน',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         ],
+          //       ),
+          //       Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.price_change_sharp)),
+          //         Text('เงินมัดจำที่ต้องจ่าย',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         Spacer(),Text('500,000 บาท',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         ],
+          //       ),
+          //       Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.car_rental)),
+          //         Text('รุ่นของรถยนต์',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         Spacer(),Text('Honda Civic RS 2024',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          //         ],
+          //       ),],),),
+          //     TextField(
+          //       decoration: InputDecoration(
+          //         border: OutlineInputBorder(),
+          //         labelText: "NAME",
+          //         hintText: "Enter Name",
+          //       ),
+          //     ),
+          //     ],
+          //   ),
+          // ),
           floatingActionButton: FloatingActionButton(onPressed: () {debugPrint('Test');},child: const Icon(Icons.shopping_bag_outlined),),
         ));
   }
 }
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Taoza'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter--;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'Count down',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
