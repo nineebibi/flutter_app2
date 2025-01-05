15ec0d7e3881e4f44e4b0484a11e0c7845043ccf
nineebibi <nattanonpak@gmail.com>
2025-01-05 17:58:31 +0700

    week5

---
+++ lib/Detail.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Detail extends StatefulWidget {
+  const Detail({super.key});
+
+  @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  String Charging_time = '';
+
+  final target = TextEditingController();
+  final chg_rate = TextEditingController();
+  final volt = TextEditingController();
+  final bat = TextEditingController();
+  final eff = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('EV Charging',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+      ),
+      body: Padding(padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset('assets/image/ev.jpg'),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                          Text(
+                            'Current SOC',
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              Text('Target SOC',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                              SizedBox(height: 10), // ระยะห่างระหว่าง Text และ TextField
+                              SizedBox(
+                                width: 150, // กำหนดขนาดความกว้างของ TextField
+                                child: TextField(
+                                  controller: target,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter Target SOC',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  onChanged: (value) {
+                                    debugPrint('Current SOC: $value');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              Text('Chg rate A',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                              SizedBox(height: 10), // ระยะห่างระหว่าง Text และ TextField
+                              SizedBox(
+                                width: 150, // กำหนดขนาดความกว้างของ TextField
+                                child: TextField(
+                                  controller: chg_rate,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter ',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  onChanged: (value) {
+                                    debugPrint('Current SOC: $value');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(onPressed: () {}, icon: const Icon(Icons.change_circle)),
+                          Text(
+                            'Volt',
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 10), // ระยะห่างระหว่าง Text และ TextField
+                          SizedBox(
+                            width: 150, // กำหนดขนาดความกว้างของ TextField
+                            child: TextField(
+                              controller: volt,
+                              decoration: InputDecoration(
+                                labelText: 'Enter Bat kWh ',
+                                border: OutlineInputBorder(),
+                              ),
+                              keyboardType: TextInputType.number,
+                              onChanged: (value) {
+                                debugPrint('Current SOC: $value');
+                              },
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_full)),
+                              SizedBox(height: 10), // ระยะห่างระหว่าง Text และ TextField
+                              SizedBox(
+                                width: 150, // กำหนดขนาดความกว้างของ TextField
+                                child: TextField(
+                                  controller: bat,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter Bat kWh ',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  onChanged: (value) {
+                                    debugPrint('Current SOC: $value');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.battery_charging_full_rounded)),
+                              SizedBox(height: 10), // ระยะห่างระหว่าง Text และ TextField
+                              SizedBox(
+                                width: 150, // กำหนดขนาดความกว้างของ TextField
+                                child: TextField(
+                                  controller: eff,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter Eff',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  onChanged: (value) {
+                                    debugPrint('Current SOC: $value');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(onPressed: () {}, icon: const Icon(Icons.alarm)),
+                          Text('Chg time(hrs) $Charging_time',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+
+    double n_Target = double.parse(target.text);
+    double n_Chg_rate = double.parse(chg_rate.text);
+    double n_Volt = double.parse(volt.text);
+    double n_Bat_kwh = double.parse(bat.text);
+    double n_Eff = double.parse(eff.text);
+
+    String _result = (n_Target * n_Bat_kwh / 100 / ((n_Volt * n_Chg_rate) * n_Eff)).toStringAsFixed(4);
+    setState(() {
+      Charging_time = "$_result";});},child: const Icon(Icons.find_in_page),),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app2/Detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,109 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-            useMaterial3: true, appBarTheme: AppBarTheme(color: Colors.yellow)),
-        home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title:Image.asset('assets/image/honda_logo.png',width: 70,),
-
-
-            // leading: IconButton(onPressed: () {debugPrint('Testiconmenu');}, icon: const Icon(Icons.menu)),
-            // actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.flag_sharp)),
-            //           IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
-            // ],
-          ),
-          body: Column(
-            children: [
-              Image.asset('assets/image/honda.jpg'),
-              Text('ขอบคุณที่ใช้บริการ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-              Text('เรายินที่ได้ใช้บิรการกับเรา Honda\n\n',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-
-              Row(children: [Text('สรุปรายละเอียดการสั่งซื้อ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))],),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.calendar_month)),
-                Text('วันที่รับรถ                                9/12/2567',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))
-                ],
-              ),
-              Row(children: [IconButton(onPressed: () {}, icon: const Icon(Icons.place)),
-                Text('สถานที่รับรถ                   สยามพารากอน',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))
-                ],
-              ),
-              // Row(
-              //   children: [
-              //     ElevatedButton(onPressed: () {debugPrint('Eiei');}, child: Text('SUBMIT')),
-              //     IconButton(onPressed: () {}, icon: Icon(Icons.favorite),iconSize: 40,),
-              //   ],
-              // ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(onPressed: () {debugPrint('Test');},child: const Icon(Icons.shopping_bag_outlined),),
-        ));
+            useMaterial3: true, appBarTheme: AppBarTheme(color: Colors.blue)),
+        home: Detail(),
+    );
   }
 }
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Taoza'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter--;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'Count down',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
